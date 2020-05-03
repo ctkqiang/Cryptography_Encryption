35a43bd82976825ec2a74570ef2d9a545278c857
John Melody Me <johnmelodyme@yandex.com>
2020-05-03 19:32:38 +0800

    Uppdated Encryption.dart

---
+++ Dart/Base64 Encryption/_decryption.dart
@@ -0,0 +1,14 @@
+import 'dart:io' as IO;
+import 'dart:convert';
+import 'dart:typed_data';
+
+void main() {}
+
+class __decrytion {
+  void __decryption_String() {
+    Codec<String, String> stringToBase64 = utf8.fuse(base64);
+    print("Please Enter {str} you want to decrypt:  ");
+    String _requestUserStringInput = IO.stdin.readLineSync();
+    // String __decodedString =  utf8.decode(base64.decode());
+  }
+}
--- Dart/Base64 Encryption/_encryption.dart
+++ Dart/Base64 Encryption/_encryption.dart
@@ -1,11 +1,10 @@
 /**
- * Copyright 2020 © John Melody Melissa
+ *    Copyright 2020 © John Melody Melissa
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  * @Author : John Melody Melissa
  * @Copyright: John Melody Melissa  © Copyright 2020
  * @INPIREDBYGF : Tan Sin Dee <3
@@ -15,16 +14,25 @@ import 'dart:io' as IO;
 import 'dart:convert';
 
 void main() {
-  print("Encrypt {string} or {file}? [input: \"str\" || \"bin\"]");
-  String _requestUserInput = IO.stdin.readLineSync();
-  if (_requestUserInput == "str") {
-    __encryption stringEncryption = new __encryption();
-    stringEncryption._encryptionString();
-  } else if (_requestUserInput == "bin") {
-    __encryption binaryEncryption = new __encryption();
-    binaryEncryption._encryptionBinary();
-  } else {
-    print("Please give a valid [Input]");
+  __onStart onStart = new __onStart();
+  onStart.__requestUser();
+}
+
+class __onStart {
+  void __requestUser() {
+    String _requestUserInput;
+    // Request $user_input either a "string" or "binary_file"
+    print("Encrypt {string} or {file}? [input: \"str\" || \"bin\"]");
+    _requestUserInput = IO.stdin.readLineSync();
+    if (_requestUserInput == "str") {
+      __encryption stringEncryption = new __encryption();
+      stringEncryption._encryptionString();
+    } else if (_requestUserInput == "bin") {
+      __encryption binaryEncryption = new __encryption();
+      binaryEncryption._encryptionBinary();
+    } else {
+      print("Please give a valid [Input]");
+    }
   }
 }
 
@@ -34,8 +42,8 @@ class __encryption {
     String _requestUserStringInput = IO.stdin.readLineSync();
     List _encodedText = utf8.encode(_requestUserStringInput);
     String _base64EncryptedString = base64.encode(_encodedText);
-    print("Original Text $_requestUserStringInput");
-    print("Encrpted String = $_base64EncryptedString");
+    print("Original Text: $_requestUserStringInput");
+    print("Encrpted String: $_base64EncryptedString");
   }
 
   Future<void> _encryptionBinary() async {
